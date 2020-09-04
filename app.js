95bbf10e6a386be194a09fc0429619695baf83f2
bc-dipesh <D.bc8829@student.leedsbeckett.ac.uk>
2020-09-04 11:53:02 +0545

    added README.md file and updated app.js to use PORT from the cofig.env file

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.use("/api/v1/lists", lists);
 // Middleware
 app.use(errorHandler);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is listening on port 3000");
 });
